e6d6bbe0da47da5732f24ff0c8d072fc533da50c
jesus.rincon@payulatam.com <8YFT`Wi~j*h@MN0$Jr9>
2017-12-27 16:06:46 -0500

    'First_code_push'

---
+++ queries.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import psycopg2 as db
+import logging
+import sys
+import datetime
+import numpy as np
+
+# Configurar Logger
+logging.basicConfig(filename='queries.log', level=logging.DEBUG)
+
+# Intentar Conexion a BD
+try:
+    conn = db.connect(dbname='pol_v4', user='readonly', host='172.18.35.22', password='YdbLByGopWPS4zYi8PIR')
+    cursor = conn.cursor()
+except:
+    logging.error('Cannot connect to database. Please run this script again')
+    sys.exit()
+
+
+# Listado de usuarios que recibieron un cambio el dia 18.
+# ARGS=fecha de ejecucion
+# -PENDIENTE AGREGAR FECHA EJECUCION
+# Agregar como variable la fecha del dia del dia anterior
+def listausuarios():
+    today = datetime.date.today()
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    print 'Searching user id with changes on ' + str(today)
+    cursor.execute("""SELECT u.usuario_id FROM audit.usuario_aud u
+    inner join audit.revision_auditoria ra ON u.rev = ra.revision_id 
+    inner join pps.usuario pu on (u.usuario_id=pu.usuario_id)
+    WHERE (to_timestamp(fecha_revision/1000))>='2017-12-18' and 
+    (to_timestamp(fecha_revision/1000))<%(today)s and pu.fecha_Creacion<%(tomorrow)s
+    order by u.usuario_id""",{'today':today,'tomorrow':tomorrow})
+    usuarios = pd.DataFrame(cursor.fetchall())
+    usuarios.columns = ['usuario_id']
+    usuarios = usuarios.drop_duplicates()
+    print 'Search finished:' + len(usuarios) + 'have received changes'
+    return usuarios
+
+
+# -------------------------------------------------------------------------------------------------------------
+
+# CONTROL DE CAMBIOS:
+# CORRE POR EL LISTADO DE USUARIOS TRAYENDO LA CONSULTA DE LOS CAMBIOS A LA FECHA Y ANEXANDO EL ULTIMO CAMBIO
+# ANTERIOR A LA FECHA DE EJECUCION DEL REPORTE.
+# RETORNA UN DATAFRAME CON EL LISTADO DE TODOS LOS CAMBIOS PARA EL LISTADO DE USUARIOS.
+
+def controlcambios(usuarios):
+
+    usuarios = usuarios['usuario_id'].tolist()
+    temp = []
+    today = datetime.date.today()
+    tomorrow=datetime.date.today() + datetime.timedelta(days=1)
+    print 'Starting check for ' + str(today)
+    audcambios = pd.DataFrame()
+    for index in range(0, len(usuarios)):
+        uid = usuarios[index]   # Indexar usuarios Id
+        print 'Checking user', uid
+        # Ejecutar consulta de fecha actual
+        cursor.execute("""SELECT to_timestamp(fecha_revision/1000) as "fec_cambio", u.rev, uw.email, 
+        u.usuario_id, u.nombres, u.nombre_contacto, u.url, u.direccion
+        FROM audit.usuario_aud u 
+        inner join audit.revision_auditoria ra ON (u.rev = ra.revision_id) 
+        inner join pps.usuario_web uw on (u.usuario_modificacion_id=uw.usuario_web_id)
+        WHERE (to_timestamp(fecha_revision/1000))>=%(today)s and (to_timestamp(fecha_revision/1000))<%(tomorrow)s
+        and u.usuario_id=%(uid)s""", {'uid': uid,'today':today,'tomorrow':tomorrow})
+        for i in xrange(cursor.rowcount):
+            temp.append(cursor.fetchone())
+        # Ejecutar consulta de cambio inmediatamente anterior
+        cursor.execute("""SELECT to_timestamp(fecha_revision/1000) as "fec_cambio", u.rev,uw.email, u.usuario_id, u.nombres, u.nombre_contacto, u.url, u.direccion
+        FROM audit.usuario_aud u
+        inner join audit.revision_auditoria ra ON u.rev = ra.revision_id 
+        inner join pps.usuario_web uw on (u.usuario_modificacion_id=uw.usuario_web_id)
+        WHERE (to_timestamp(fecha_revision/1000))<%(today)s and u.usuario_id=%(uid)s
+        order by "fec_cambio" desc limit 1""", {'uid': uid, 'today':today})
+        i = 0
+        for i in xrange(cursor.rowcount):
+            temp.append(cursor.fetchone())
+        cambios = pd.DataFrame(temp,columns = ['fecha_rev', 'rev_id', 'email', 'usuario_id', 'nombre_comercio', 'nombre_contacto', 'url',
+                           'domicilio'])
+        cambios['fecha_rev'] = pd.to_datetime(cambios.fecha_rev)
+        cambios = cambios.sort_values(by='fecha_rev')
+        cambiosord2 = cambios.shift()
+        cambiosord2.columns = ['fecha_revprev', 'rev_idprev', 'emailprev', 'usuario_idprev', 'nombre_comercioprev',
+                               'nombre_contactoprev', 'urlprev','domicilioprev']
+        df = pd.concat([cambios, cambiosord2], axis=1)
+        # En caso de que sea un primer cambio, ejecutar script de identificacion
+        if len(df)!=0:
+            df.drop(df.index[0], inplace=True)
+            df['cambio'] = np.where(df['nombre_contacto'] != df['nombre_contactoprev'], 'nombre_contacto',
+                                np.where(df['nombre_comercio'] != df['nombre_comercioprev'], 'nombre_comercio',
+                                         np.where(df['url'] != df['urlprev'], 'url',
+                                                  np.where(df['domicilio'] != df['domicilioprev'], 'domicilio', None))))
+            df = df[df.cambio.notnull()]
+            audcambios = audcambios.append(df, ignore_index=True)
+        print 'Found', len(df), 'changes'
+
+        # Limpiar variables iterativas
+        df = df.iloc[0:0]
+        cambios=cambios.iloc[0:0]
+        cambiosord2=cambiosord2.iloc[0:0]
+        temp=[]
+        print 'User Id',uid,'finished'
+    # Eliminar Columnas innecesarias
+    return audcambios
+#-------------------------------------------------------------------------------------------------------
+
+# Taer listado de usuarios
+users = listausuarios()
+
+# Ejecutar script de control de cambios
+cambios = controlcambios(users)
+
+# Guardar Cambios en archivo de Excel
+today = datetime.date.today()
+filename = 'Users_Report_' + str(today) + ('.xlsx')
+writer = pd.ExcelWriter(filename)
+cambios.to_excel(writer)
+writer.save()
---
+++ reports-engine.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import psycopg2 as db
+import logging
+import sys
+import datetime
+try:
+    conn = db.connect(dbname='pol_v4', user='readonly', host='172.18.35.22', password='YdbLByGopWPS4zYi8PIR')
+    cursor = conn.cursor()
+except:
+    logging.error('Cannot connect to database. Please run this script again')
+    sys.exit()
+
+uid=500199
+date=datetime.date.today()
+date2=datetime.date.today() - datetime.timedelta(days=1)
+tomorrow=datetime.date.today() + datetime.timedelta(days=1)
+print tomorrow
+print date
+print date2
+cursor.execute("""select * from pps.transaccion where usuario_id=%(uid)s and fecha_creacion>=%(date)s limit 1""",
+               {'uid': uid,'date':date})
+print cursor.fetchone()
+cambios=pd.DataFrame({'B': [9, 8, 7, 6, 5, 4]})
+
+print cambios
+
+name='output'+str(date)+'.xlsx'
+writer = pd.ExcelWriter(name)
+cambios.to_excel(writer)
+writer.save()
